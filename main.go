c092d97e67b9e0d7f13d1f7002465f8183ec6107
wu11287 <while11287@163.com>
2021-06-01 11:55:13 +0800

    rnd broadcast

--- main.go
+++ main.go
@@ -20,7 +20,8 @@ type node struct {
 	rnd    crypto.VrfOutput
 	proof  crypto.VrfProof
 	pkList []crypto.VrfPubkey
-	// ch chan crypto.VrfPubkey //管道
+	proofList []crypto.VrfProof
+	// ch chan crypto.VrfPubkey
 }
 
 //生成长度为 n 的随机byte数组
@@ -55,115 +56,111 @@ func newNode(nNode *node, i int) {
 }
 
 //每个节点把自己的公钥写到所有节点管道中 -- 此时公钥是已经生成的
-func broadcastPK(ch []chan crypto.VrfPubkey, nNode *node, i int, deferFunc func()) {
+func broadcastPK(ch []chan crypto.VrfPubkey, nNode *node, deferFunc func()) {
 	defer func() {
 		deferFunc()
 	}()
-	// for i:= 0; i < 4; i++ {
-	// 	select {
-	// 	case ch[0] <- nNode.pk:
-	// 		fmt.Printf("node %d sendPK to ch[0] success!", i)
-	// 	case ch[1] <- nNode.pk:
-	// 		fmt.Printf("node %d sendPK to ch[1] success!", i)
-	// 	case ch[2] <- nNode.pk:
-	// 		fmt.Printf("node %d sendPK to ch[2] success!", i)
-	// 	case ch[3] <- nNode.pk:
-	// 		fmt.Printf("node %d sendPK to ch[3] success!", i)
-	// 	default:
-	// 		fmt.Printf("broadcast pk error\n")
-	// 	}
-	// }
-	// for i := 0; i < 4; i++ {
-	// 	ch[i] <- nNode.pk //数据存在管道里
-	// 	close(ch[i])
-	// }
 	ch[0] <- nNode.pk
 	ch[1] <- nNode.pk
 	ch[2] <- nNode.pk
 	ch[3] <- nNode.pk
 }
 
-// func broadcastProof(ch []chan interface{}, nNode *node, i int, deferFunc func()) {
-// 	defer func(){
-// 		deferFunc()
-// 	}()
-// 	for {
-// 		select {
-// 		case ch[0] <- nNode.proof:
-// 			fmt.Printf("node %d sendProof to ch[0] success!", i)
-// 		case ch[1] <- nNode.pk:
-// 			fmt.Printf("node %d send ch[1] success!", i)
-// 		case ch[2] <- nNode.pk:
-// 			fmt.Printf("node %d send ch[2] success!", i)
-// 		case ch[3] <- nNode.pk:
-// 			fmt.Printf("node %d send ch[3] success!", i)
-// 		default:
-// 			fmt.Printf("broadcast pk error")
-// 		}
-// 	}
-// }
+//持久化公钥到节点struct
+func storePk(ch chan crypto.VrfPubkey, nNode *[4]node, id int) {
+	for i := 0; i < 4; i++ {
+		val0 := <- ch
+		nNode[id].pkList = append(nNode[id].pkList, val0)
+	}
+}
+
+func storeProof(ch chan crypto.VrfProof, nNode *[4]node, id int) {
+	for i := 0; i < 4; i++ {
+		val0 := <- ch
+		nNode[id].proofList = append(nNode[id].proofList, val0)
+	}
+}
+
+func broadcastProof(ch []chan crypto.VrfProof, nNode *node, deferFunc func()) {
+	defer func(){
+		deferFunc()
+	}()
+	ch[0] <- nNode.proof
+	ch[1] <- nNode.proof
+	ch[2] <- nNode.proof
+	ch[3] <- nNode.proof
+}
+
+func broadcastRnd(ch []chan crypto.VrfOutput, nNode *node, deferFunc func()) {
+	defer func(){
+		deferFunc()
+	}()
+	if !isMeet(nNode) {  //不满足条件直接返回
+		return
+	}
+	ch[0] <- nNode.rnd
+	ch[1] <- nNode.rnd
+	ch[2] <- nNode.rnd
+	ch[3] <- nNode.rnd
+}
 
 func main() {
 	var wg sync.WaitGroup
-	chs := make([]chan crypto.VrfPubkey, 4) //四个节点的管道切片,管道传送公钥
+	chsPK := make([]chan crypto.VrfPubkey, 4) //四个节点的管道切片,管道传送公钥
+	chsProof := make([]chan crypto.VrfProof, 4)
+	chsRnd := make([]chan crypto.VrfOutput, 4)
 	nodes := [4]node{}
 	rand.Seed(time.Now().Unix()) //以当前时间，更新随机种子
 	randomness, err := GenerateRandomBytes(10)
 	if err != nil {
 		fmt.Println("generate random byte[] failed!")
 	}
-	fmt.Printf("%#v\n", randomness)
 	//生成所有节点的公私钥
 	for i := 0; i < 4; i++ {
-		chs[i] = make(chan crypto.VrfPubkey) //实例化这个管道
+		chsPK[i] = make(chan crypto.VrfPubkey) //实例化这个管道
+		chsProof[i] = make(chan crypto.VrfProof)
+		chsRnd[i] = make(chan crypto.VrfOutput)
 		wg.Add(1)
 		newNode(&nodes[i], i)
 	}
 
 	// 广播公钥
 	for i := 0; i < 4; i++ {
-		broadcastPK(chs, &nodes[i], i, wg.Done)
+		go broadcastPK(chsPK, &nodes[i], wg.Done)
 	}
 
-	//把每个管道中的公钥，持久化到每个节点的pklist中
 	for i := 0; i < 4; i++ {
-		val := <- chs[0]
-		nodes[0].pkList = append(nodes[0].pkList, val)
+		go storePk(chsPK[i], &nodes, i)
 	}
+	
+	//加密抽签
 	for i := 0; i < 4; i++ {
-		val := <- chs[1]
-		nodes[1].pkList = append(nodes[1].pkList, val)
+		go Sotition(&nodes[i], randomness)
 	}
+	//广播Proof -- 应该先生成proof
 	for i := 0; i < 4; i++ {
-		val := <- chs[2]
-		nodes[2].pkList = append(nodes[2].pkList, val)
+		proof, ok1 := nodes[i].sk.ProveMy(randomness)
+		if !ok1 {
+			fmt.Println("proof generated error")
+		}
+		rnd, ok2 := proof.Hash()
+		if !ok2 {
+			fmt.Println("output generated error")
+		}
+		nodes[i].proof = proof
+		nodes[i].rnd = rnd
+		go broadcastProof(chsProof, &nodes[i], wg.Done)
+		go broadcastRnd(chsRnd, &nodes[i], wg.Done)
 	}
+
+
 	for i := 0; i < 4; i++ {
-		val := <- chs[3]
-		nodes[3].pkList = append(nodes[3].pkList, val)
+		wg.Add(1)
+		go storeProof(chsProof[i], &nodes, i)
 	}
-	// for val := range chs[1] {
-	// 	<-chs[1]
-	// 	nodes[1].pkList = append(nodes[1].pkList, val)
-	// }
 
 	wg.Wait()
 
-	for _, val := range nodes[0].pkList {
-		fmt.Print(val, " ")
-	}
-	for _, val := range nodes[1].pkList {
-		fmt.Print(val, " ")
-	}
-
-	// //加密抽签
-	// for i := 0; i < 4; i++ {
-	// 	go Sotition(&nodes[i], randomness)
-	// }
-	// //广播Proof
-	// for i := 0; i < 4; i++ {
-	// 	broadcastProof(chs, &nodes[i], i, wg.Done)
-	// }
 
 	// identified := make([]int, 0)
 	// for i := 0; i < 4; i++ {
